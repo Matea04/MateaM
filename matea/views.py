d9245a51482a3bab759cef90b63c822797cae4a3
Matea <mateamihaljevic@ssklivno.net>
2022-06-01 08:31:13 +0200

    views

--- matea/views.py
+++ matea/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from .models import Choice, Question
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.urls import reverse
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.shortcuts import  render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from .forms import CommentForm
+from .models import Post
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'matea/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+             pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+    def post(self,request ,slug):
+        comment_form = CommentForm(request.POST)
+        post=Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment=comment_form.save(commit=False)
+            comment.post= post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page",args=[slug]))
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'matea/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'matea/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'matea/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('matea:results', args=(question.id,)))
+   
+ 
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("matea:index")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="matea/register.html", context={"register_form":form})
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("matea:index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="matea/login.html", context={"login_form":form})
+
