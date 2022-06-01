9e111f2a4810eeb178e8ee10c9da96364b06a69f
Matea <mateamihaljevic@ssklivno.net>
2022-06-01 08:35:28 +0200

    settings

--- MateaM/settings.py
+++ MateaM/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'matea.apps.MateaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'MateaM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             BASE_DIR + '/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR + '/static'
+]
+
+MEDIA_ROOT = BASE_DIR + '/uploads' #Direktorij put do slike
+MEDIA_URL="/files/" 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
